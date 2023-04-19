2c959c27f3604940fc53980d9a40efcfe9951eee
PurpleLemon18 <54015019+PurpleLemon18@users.noreply.github.com>
2023-04-19 17:33:55 +1200

    Q1: Complete Q2: copied Banks, Robberies and Plans

---
+++ Q1.sql
@@ -0,0 +1,132 @@
+/* 
+Banks
+Attributes: BankName, City, NoAccounts, Security
+Primary key: BankName, City
+Foreign key: none
+Constraints: NoAccounts > 0
+*/
+CREATE TABLE Banks (
+    BankName VARCHAR(50) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    NoAccounts INT CHECK (NoAccounts > 0),
+    Security VARCHAR(50),
+    PRIMARY KEY (BankName, City)
+);
+
+/*
+Robberies
+Attributes: BankName, City, Date, Amount
+Primary key: BankName, City, Date
+Foreign key: BankName, City (Banks)
+Constraints: Amount > 0
+*/
+CREATE TABLE Robberies (
+    BankName VARCHAR(50) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    Date DATE NOT NULL,
+    Amount DECIMAL (10, 2) CHECK (Amount > 0),
+    PRIMARY KEY (BankName, City, Date),
+    FOREIGN KEY (BankName, City) REFERENCES Banks (BankName, City)
+);
+
+/*
+Plans 
+Attributes: BankName, City, NoRobbers, PlannedDate
+Primary key: BankName, City, PlannedDate
+Foreign key: BankName, City (Banks)
+Constraints: NoRobbers > 0
+*/
+CREATE TABLE Plans (
+    BankName VARCHAR(50) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    NoRobbers INT CHECK (NoRobbers > 0),
+    PlannedDate DATE NOT NULL,
+    PRIMARY KEY (BankName, City, PlannedDate),
+    FOREIGN KEY (BankName, City) REFERENCES Banks (BankName, City)
+);
+
+/*
+Robbers
+Attributes: RobberId, Nickname, Age, NoYears
+Primary key: RobberId
+Foreign key: none
+Constraints: Age > 0, NoYears < Age
+*/
+CREATE TABLE Robbers (
+    RobberId INT NOT NULL,
+    Nickname VARCHAR(50),
+    Age INT CHECK (Age > 0),
+    NoYears INT CHECK (NoYears < Age),
+    PRIMARY KEY (RobberId)
+);
+
+/*
+Skills
+Attributes: SkillId, Description
+Primary key: SkillId
+Foreign key: none
+Constraints: Description is not null
+*/
+CREATE TABLE Skills (
+    SkillId INT NOT NULL,
+    Description VARCHAR(50) NOT NULL,
+    PRIMARY KEY (SkillId)
+);
+
+/*
+HasSkills
+Attributes: RobberId, SkillId, Preference, Grade
+Primary key: RobberId, SkillId
+Foreign key: RobberId (Robbers), SkillId (Skills)
+Constraints: Preference > 0
+*/
+CREATE TABLE HasSkills (
+    RobberId INT NOT NULL,
+    SkillId INT NOT NULL,
+    Preference INT CHECK (Preference > 0),
+    GRADE VARCHAR(50),
+    PRIMARY KEY (RobberId, SkillId),
+    FOREIGN KEY (RobberId) REFERENCES Robbers (RobberId),
+    FOREIGN KEY (SkillId) REFERENCES Skills (SkillId)
+);
+
+/*
+HasAccounts
+Attributes: RobberId, BankName, City
+Primary key: RobberId, BankName, City
+Foreign key: RobberId (Robbers), BankName, City (Banks)
+Constraints: none
+*/
+CREATE TABLE HasAccounts (
+    RobberId INT NOT NULL,
+    BankName VARCHAR(50) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    PRIMARY KEY (RobberId, BankName, City),
+    FOREIGN KEY (RobberId) REFERENCES Robbers (RobberId),
+    FOREIGN KEY (BankName, City) REFERENCES Banks (BankName, City)
+);
+
+/*
+Accomplices
+Attributes: RobberId, BankName, City, Date, Share
+Primary key: RobberId, BankName, City, Date
+Foreign key: RobberId (Robbers), BankName, City, Date (Robberies)
+Constraints: none
+*/
+CREATE TABLE Accomplices (
+    RobberId INT NOT NULL, 
+    BankName VARCHAR(50) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    Date DATE NOT NULL,
+    Share INT,
+    PRIMARY KEY (RobberId, BankName, City, Date),
+    FOREIGN KEY (RobberId) REFERENCES Robbers (RobberId),
+    FOREIGN KEY (BankName, City, Date) REFERENCES Robberies (BankName, City, Date)
+);
+
+/*
+Changes:
+- Changed Amount (Robberies) from INTEGER to type DECIMAL (10, 2) 
+ALTER TABLE Robberies
+ALTER COLUMN Amount TYPE DECIMAL (10, 2);
+*/
---
+++ Q2.sql
@@ -0,0 +1,3 @@
+\copy Banks FROM /am/kings/home1/gajadhroha/Documents/SWEN304/banks_23.data
+\copy Robberies FROM /am/kings/home1/gajadhroha/Documents/SWEN304/robberies_23.data
+\copy Plans(BankName, City, PlannedDate, NoRobbers) FROM /am/kings/home1/gajadhroha/Documents/SWEN304/plans_23.data
